520885fd65d857ef0e287e0283ba36690f2747bb
caanerud <caanerud.ca@gmail.com>
2017-03-09 16:44:25 -0500

    pushing to github

--- src/main/java/com/theironyard/Main.java
+++ src/main/java/com/theironyard/Main.java
@@ -3,24 +3,28 @@ package com.theironyard;
 /**
  * For this exercise you will write a simple main() method that prints out the
  * following text:
- *
+ * <p>
  * 1
  * 2 2
  * 4 4 4 4
  * 5 5
  * 6 6 6 6 6 6
- *
+ * <p>
  * Note the following facts:
- *
+ * <p>
  * - 3 is not output at all
  * - 5 is only output twice
  * - You are required use nested for loops (not while loops)
  * - You will need to use both the "break" and continue "keywords"
  * - Each number is followed by a space. EG: 2_2_, not 2_2.
- *
+ * <p>
  * The test is picky about formatting so make sure you get it right. Ask
  * questions if something is unclear.
- *
+ * 1
+ * 2 2
+ * 4 4 4 4
+ * 5 5
+ * 6 6 6 6 6 6
  */
 public class Main {
 
@@ -35,7 +39,23 @@ public class Main {
          */
         // todo: output the expected text
 
+        for (int x = 1; x <= 6; x++) {
+            if (x == 3) {
+                continue;
+            }
+            if (x != 1) {
+                System.out.println();
+            }
+            for (int y = 1; y <= x; y++) {
+                if (y > 2 && x == 5) {
+                    break;
+                }
+                System.out.print(x + " ");
+            }
 
+
+        }
+        System.out.println();
     }
-
 }
+
